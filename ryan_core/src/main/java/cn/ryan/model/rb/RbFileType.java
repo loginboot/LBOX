5ecfa8b04322bcd8bdeb05635d60fb1f84324dff
xiesw <xiesw@LOGINBOOT-PC>
2019-11-10 11:49:15 +0800

    文件类型model

--- ryan_core/src/main/java/cn/ryan/model/rb/RbFileType.java
+++ ryan_core/src/main/java/cn/ryan/model/rb/RbFileType.java
@@ -1,5 +1,44 @@
 package cn.ryan.model.rb;
 
+/**
+ * 
+ * @author cn.ryan
+ * @creator xiesw
+ * @version 1.0.0
+ * @date 2019-11-10
+ * @description 文件类型表模型
+ *
+ */
 public class RbFileType {
+
+	private int id;
+
+	private String name;
+
+	private String pattern;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
 
 }
